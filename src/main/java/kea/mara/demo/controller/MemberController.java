c6094303ff31814c1301c3eeba71f46500e1db96
MadsRasted <mads95e3@stud.kea.dk>
2020-09-11 11:11:07 +0200

    Connected to DB

--- src/main/java/kea/mara/demo/controller/MemberController.java
+++ src/main/java/kea/mara/demo/controller/MemberController.java
@@ -10,26 +10,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MemberController {
 
     @Autowired
     IMemberRepository memberRepository;
+/*
+    public MemberController(IMemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+*/
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpSession session){
+
+        if(session.getAttribute("isLoggedIn") != null){
+            return "secret";
+        }
         return "index";
     }
 
     @PostMapping("/")
-    public String login(@ModelAttribute Member member, Model model){
+    public String login(@ModelAttribute Member member, Model model, HttpSession session){
         //Check if crediatils is in the arrayList
         Member m = memberRepository.read(member.getEmail());
         if(m != null){
+            session.setAttribute("isLoggedIn", "yes");
             model.addAttribute("members", memberRepository.readAll());
             return "secret";
         }
         return "index";
+    }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+
+        session.removeAttribute("isLoggedIn");
+        return "index";
     }
 }
